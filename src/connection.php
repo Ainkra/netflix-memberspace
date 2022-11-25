d9296d1cb8220cfdcf86800596a42b98f3f7e059
Ainkra <felke2101@gmail.com>
2022-11-25 15:03:53 +0100

    style: fix indent

--- src/connection.php
+++ src/connection.php
@@ -1,7 +1,7 @@
 <?php
-
-    try {
-        $db = new PDO("mysql:host=localhost;dbname=netflix;charset=utf8", "root", "");
-    } catch (Exception $e) {
-        die("Erreur: ".$e->getMessage());
-    }
+try {
+    $db = new PDO("mysql:host=localhost;dbname=netflix;charset=utf8", "root", "");
+} catch (Exception $e) {
+    header("location: index.php?error=1&message='Connexion échouée : Impossible de communiquer avec la base de donnée'");
+}
+?>
